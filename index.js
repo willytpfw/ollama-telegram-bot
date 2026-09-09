26bf8548e36bfcaa12070c94f107c789e3e4a1c1
willytpfw <willytpfw@gmail.com>
2026-09-09 13:02:05 -0600

    Fix error URL API

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ async function pollLoop() {
         log('Consulta recibida en chat', msg.chat.id, '->', query);
 
         if (query === "Dame mi Token") {
-          const token = await callAPI("http://localhost:5173/api/auth/authenticate?userLogin=willytpfw&password=Dejamelo1$")
+          const token = await callAPI("http://app.tpfw.com.mx/api/auth/authenticate?userLogin=willytpfw&password=Dejamelo1$")
           await sendMessage(msg.chat.id, "Token: " + token, msg.message_id);
           log('Token enviado.');
           continue;
