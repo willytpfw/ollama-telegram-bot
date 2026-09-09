389017f6a7639178d90e64c64d156d29fb7912c4
willytpfw <willytpfw@gmail.com>
2026-09-09 13:48:43 -0600

    Fix error reference json

--- index.js
+++ index.js
@@ -130,7 +130,10 @@ async function pollLoop() {
         try {
           if (query === "Dame mi Token") {
             const msgAPI = await callAPI("http://app.tpfw.com.mx/api/auth/authenticate?userLogin=willytpfw&password=Dejamelo1$", "GET");
-            await sendMessage(msg.chat.id, "Token: " + JSON.stringify(msgAPI.data.Token), msg.message_id);
+
+            const jsonString = JSON.stringify(msgAPI);
+            const obj = JSON.parse(jsonString);
+            await sendMessage(msg.chat.id, "Token: " + obj.Token, msg.message_id);
             log('Token enviado.');
             continue;
           }
